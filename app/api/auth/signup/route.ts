e96a59f7d73df581a8b8ad7fa141c0321b8a2705
Shivraj Khanekar <shivrajkhanekar@Shivrajs-MacBook-Air.local>
2026-03-28 14:09:09 +0530

    authentication

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -49,7 +49,7 @@ export async function POST(req: Request) {
     response.cookies.set("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "lax",
       maxAge: 60 * 60 * 24 * 7, // 1 week
       path: "/",
     });
